6d7137f07cbc29d42d75668de221d6412d55c303
Thụy Nguyễn <thuynv06@gmail.com>
2022-11-21 22:04:06 +0800

    refactor code 1

--- backend/filterWithCookie.php
+++ backend/filterWithCookie.php
@@ -5,10 +5,10 @@
         if($checkCookie['role']==1){
             echo '<a href="admin/production/shoe.php">TRUY CẬP TRANG ADMIN</a>';
         }else if ($checkCookie['role']==0){
-            echo '<a href="../thlogistics/danhsachdonhang.php">Quản Lý Kiện Hàng</a>';
+            echo '<a href="danhsachdonhang.php">Quản Lý Kiện Hàng</a>';
         }
-        echo '<a href="../thlogistics/profile.php">'.$checkCookie['fullname'].'</a>
-        <a href="/thlogistics/backend/logoutCookie.php">Đăng Xuất</a>';
+        echo '<a href="profile.php">'.$checkCookie['fullname'].'</a>
+        <a href="backend/logoutCookie.php">Đăng Xuất</a>';
     }
     else{
         echo '<a href="auth/login/index.php">Đăng Nhập & Đăng Ký</a>';
