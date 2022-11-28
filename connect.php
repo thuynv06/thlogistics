850999bdb1907b62e40a5c00071a99e37be80feb
Thụy Nguyễn <thuynv06@gmail.com>
2022-11-28 21:22:54 +0800

    okkkkkkk

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
-$server = "localhost:3306";
-$user="root";
-$pass="";
-$database="thlogistics";
-$conn=mysqli_connect($server,$user,$pass,$database);
 //$server = "localhost:3306";
-//$user="Root";
-//$pass="Root_1234";
+//$user="root";
+//$pass="";
 //$database="thlogistics";
 //$conn=mysqli_connect($server,$user,$pass,$database);
+$server = "localhost:3306";
+$user="Root";
+$pass="Root_1234";
+$database="thlogistics";
+$conn=mysqli_connect($server,$user,$pass,$database);
 // Check connection
 //if ($conn->connect_error) {
 //    die("Connection failed: " . $conn->connect_error);
