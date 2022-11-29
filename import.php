ec432b430a10db4fac2482d7cf5ccbfda2ed4a1d
Thụy Nguyễn <thuynv06@gmail.com>
2022-11-30 06:11:43 +0800

    big updated

--- import.php
+++ import.php
@@ -31,53 +31,53 @@ if (isset($_POST["btnImport"])) {
         $sheetCount = count($spreadSheetAry);
 //        echo $sheetCount;
 // output the data to the console, so you can see what there is.
-        die(print_r($spreadSheetAry, true));
+//        die(print_r($spreadSheetAry, true));
 
 
-//        for ($i = 1; $i < $sheetCount - 2; $i++) {
-//            $ladingCode = "";
-//            if (isset($spreadSheetAry[$i][0])) {
-//                $ladingCode = mysqli_real_escape_string($conn, $spreadSheetAry[$i][0]);
-//            }
-////            echo $ladingCode;
-//            $name = "";
-//            if (isset($spreadSheetAry[$i][1])) {
-//                $name = mysqli_real_escape_string($conn, $spreadSheetAry[$i][1]);
-//            }
-//            $nametq = "";
-//            if (isset($spreadSheetAry[$i][2])) {
-//                $nametq = mysqli_real_escape_string($conn, $spreadSheetAry[$i][2]);
-//            }
-//            $amount = $spreadSheetAry[$i][3];
-//            $price = $spreadSheetAry[$i][4];
-//            $size = $spreadSheetAry[$i][5];
-//            $linksp = "";
-//            if (isset($spreadSheetAry[$i][6])) {
-//                $linksp = mysqli_real_escape_string($conn, $spreadSheetAry[$i][6]);
-//            }
-//            $note = "";
-//            if (isset($spreadSheetAry[$i][7])) {
-//                $note = mysqli_real_escape_string($conn, $spreadSheetAry[$i][7]);
-//            }
-//
-////            if (! empty($name) || ! empty($description)) {
-//            $date = new DateTime();
-//            $dateCreadted = $date->format("Y-m-d\TH:i:s");
-//            $myObj = new stdClass();
-//            $myObj->{1} = "$dateCreadted";
-//            $listStatusJSON = json_encode($myObj);
-//            $kienhang_id = $kienhangRepository->insert($name, $nametq, $ladingCode, $amount, "BT / HN1", $size, 1, $price, $user_id, $linksp, $note, $dateCreadted, $listStatusJSON);
-//            $kienhangRepository->updateMaKien($kienhang_id);
-//
-//            if (!empty($kienhang_id)) {
-//                $type = "success";
-//                $message = "Excel Data Imported into the Database";
-//            } else {
-//                $type = "error";
-//                $message = "Problem in Importing Excel Data";
-//            }
-//        }
-//        echo "<script>alert('Thêm thành công');window.location.href='danhsachdonhang.php';</script>";
+        for ($i = 1; $i < $sheetCount - 2; $i++) {
+            $ladingCode = "";
+            if (isset($spreadSheetAry[$i][0])) {
+                $ladingCode = mysqli_real_escape_string($conn, $spreadSheetAry[$i][0]);
+            }
+//            echo $ladingCode;
+            $name = "";
+            if (isset($spreadSheetAry[$i][1])) {
+                $name = mysqli_real_escape_string($conn, $spreadSheetAry[$i][1]);
+            }
+            $nametq = "";
+            if (isset($spreadSheetAry[$i][2])) {
+                $nametq = mysqli_real_escape_string($conn, $spreadSheetAry[$i][2]);
+            }
+            $amount = $spreadSheetAry[$i][3];
+            $price = $spreadSheetAry[$i][4];
+            $size = $spreadSheetAry[$i][5];
+            $linksp = "";
+            if (isset($spreadSheetAry[$i][6])) {
+                $linksp = mysqli_real_escape_string($conn, $spreadSheetAry[$i][6]);
+            }
+            $note = "";
+            if (isset($spreadSheetAry[$i][7])) {
+                $note = mysqli_real_escape_string($conn, $spreadSheetAry[$i][7]);
+            }
+
+//            if (! empty($name) || ! empty($description)) {
+            $date = new DateTime();
+            $dateCreadted = $date->format("Y-m-d\TH:i:s");
+            $myObj = new stdClass();
+            $myObj->{1} = "$dateCreadted";
+            $listStatusJSON = json_encode($myObj);
+            $kienhang_id = $kienhangRepository->insert($name, $nametq, $ladingCode, $amount, "BT / HN1", $size, 1, $price, $user_id, $linksp, $note, $dateCreadted, $listStatusJSON);
+            $kienhangRepository->updateMaKien($kienhang_id);
+
+            if (!empty($kienhang_id)) {
+                $type = "success";
+                $message = "Excel Data Imported into the Database";
+            } else {
+                $type = "error";
+                $message = "Problem in Importing Excel Data";
+            }
+        }
+        echo "<script>alert('Thêm thành công');window.location.href='danhsachdonhang.php';</script>";
     }
 } else {
     $type = "error";
