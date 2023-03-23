5dbf37d64b4ef935c59e22a188c6ccd9b884845a
thuynv06 <thuynv06@gmail.com>
2023-03-23 12:00:15 +0700

    edditphieu

--- admin/production/phieuxuatkho.php
+++ admin/production/phieuxuatkho.php
@@ -33,41 +33,61 @@ function phieuxuatkho($listId,$userID)
 //    echo "Ngày: ".$date['mday']."<hr>";
 //    echo "Tháng: ".$date['mon']."<hr>";
 //    echo "Năm: ".$date['year']."<hr>";
-        $ngaythang = "Ngày " . $date['mday'] . " Tháng " . $date['mon'] . " Năm " . $date['year'];
+        $ngaythang = "(Ngày " . $date['mday'] . " Tháng " . $date['mon'] . " Năm " . $date['year'].")";
 
-        $sheet->mergeCells('A1:E1');
 
-        $sheet->setCellValue('A1', mb_strtoupper("Phiếu Xuất Kho", "UTF-8"));
-        $sheet->getStyle('A1:E1')->getFont()->setBold(true);
-        $sheet->getStyle('A1:E1')->getFont()->setSize(15);
-        $sheet->getRowDimension(1)->setRowHeight(
-            20 * (substr_count($sheet->getCell('A1')->getValue(), "\n") + 1)
-        );
+        $sheet->setCellValue('A1', mb_strtoupper("Công Ty TNHH Thương Mại Dịch Vụ Trung Hoa Logistics", "UTF-8"));
+        $sheet->mergeCells('A1:F1');
+//        $sheet->getStyle('A1:G1')->getFont()->setSize(14);
+//        $sheet->getRowDimension(1)->setRowHeight(
+//            20 * (substr_count($sheet->getCell('A1')->getValue(), "\n") + 1)
+//        );
+
+        $sheet->getStyle('A1:G1')->getFont()->setBold(true);
+        $sheet->setCellValue('A2', ("Địa Chỉ: Ngõ 15 - Phố Ba La - Tổ 10 Phường Phú La - Hà Đông - Hà Nội"));
+        $sheet->setCellValue('A3', ("Hotline: 033.699.1688 - 0399.322.668"));
+
+        $sheet->setCellValue('A5', mb_strtoupper("Phiếu Xuất Kho", "UTF-8"));
+        $sheet->mergeCells('A5:G5');
+        $sheet->getStyle('A5:G5')->getAlignment()->setHorizontal('center');
+
+        $sheet->getStyle('A5:E5')->getFont()->setBold(true);
+        $sheet->getStyle('A5:E5')->getFont()->setSize(15);
+//        $sheet->getRowDimension(1)->setRowHeight(
+//            20 * (substr_count($sheet->getCell('A4:A5')->getValue(), "\n") + 1)
+//        );
 
 
 //    $sheet->setCellValue('B1', "Tên Sản Phẩm");
-        $sheet->setCellValue('F1', "Số Phiếu:");
-        $sheet->getStyle('F1')->getAlignment()->setHorizontal('right');
-        $sheet->setCellValue('G1', $kh['code']);
-        $sheet->setCellValue('A2', $ngaythang);
+        $sheet->setCellValue('G1', "Số Phiếu:".$kh['code']);
+//        $sheet->getStyle('1')->getAlignment()->setHorizontal('right');
+//        $sheet->setCellValue('G5', $kh['code']);
+        $sheet->setCellValue('A6', $ngaythang);
+        $sheet->mergeCells('A6:G6');
+        $sheet->getStyle('A6:G6')->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('A6:G6')->getFont()->setSize(11);
+        $sheet->getStyle('A6:G6')->getFont()->setItalic(true);
 
-        $sheet->setCellValue('A4', "Khách Hàng: " . strtoupper($kh['code']) . " - " . mb_strtoupper($kh['fullname'], "UTF-8"));
-        $sheet->getStyle('A4:G4')->getFont()->setBold(true);
+        $sheet->setCellValue('A8', "Khách Hàng: " . strtoupper($kh['code']) . " - " . mb_strtoupper($kh['fullname'], "UTF-8"));
+        $sheet->getStyle('A8:G8')->getFont()->setBold(true);
+        $sheet->setCellValue('A9',"Điện thoại: ".$kh['phone']);
+        $sheet->setCellValue('A10',"Email: ".$kh['email']);
+        $sheet->setCellValue('A11',"Địa Chỉ: ".$kh['address']);
 
 
-        $sheet->setCellValue('A6', "STT");
-        $sheet->setCellValue('B6', "Mã Vận Đơn");
-        $sheet->setCellValue('C6', "TQ Nhận");
-        $sheet->setCellValue('D6', "VN Nhận");
-        $sheet->setCellValue('E6', "Cân Nặng (Kg) ");
-        $sheet->setCellValue('F6', "Đơn Giá (đ)");
-        $sheet->setCellValue('G6', "Thành Tiền (đ)");
+            $sheet->setCellValue('A13', "STT");
+        $sheet->setCellValue('B13', "Mã Vận Đơn");
+        $sheet->setCellValue('C13', "TQ Nhận");
+        $sheet->setCellValue('D13', "VN Nhận");
+        $sheet->setCellValue('E13', "Cân Nặng (Kg) ");
+        $sheet->setCellValue('F13', "Đơn Giá (VNĐ)");
+        $sheet->setCellValue('G13', "Thành Tiền (VNĐ)");
 //Make a bottom border(optional).
-        $sheet->getStyle('A6:G6')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->getStyle('A13:G13')->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
 //Set header background color(optional).
-        $sheet->getStyle('A6:G6')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('d2d3d1');
+        $sheet->getStyle('A13:G13')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('d2d3d1');
 //Set text bold.
-        $sheet->getStyle('A6:G6')->getFont()->setBold(true);
+        $sheet->getStyle('A13:G13')->getFont()->setBold(true);
 //Set auto resize(optional).
         $sheet->getColumnDimension('A')->setWidth(30, 'pt');
         $sheet->getColumnDimension('B')->setAutoSize(true);
@@ -81,7 +101,7 @@ function phieuxuatkho($listId,$userID)
         //Write data 1.
         $tongcan = 0;
         $tongtienvc = 0;
-        $i = 7;
+        $i = 14;
         foreach ($listId as $product_id) {
 
             $tempProduct = $kienhangRepository->getById($product_id)->fetch_assoc();
@@ -97,7 +117,7 @@ function phieuxuatkho($listId,$userID)
                     $vnnhan = date('Y-m-d', strtotime($obj->{4}));
                 }
             }
-            $sheet->setCellValue('A' . $i, $i - 6);
+            $sheet->setCellValue('A' . $i, $i - 13);
             $sheet->setCellValue('B' . $i, $tempProduct['ladingCode']);
             $sheet->setCellValue('C' . $i, $tqnhan);
             $sheet->setCellValue('D' . $i, $vnnhan);
@@ -117,17 +137,17 @@ function phieuxuatkho($listId,$userID)
 //Set header background color(optional).
         $sheet->getStyle('E' . $i . ':G' . $i)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('d2d3d1');
         $sheet->getStyle('E' . $i . ':G' . $i)->getNumberFormat()->setFormatCode('#,##');
-        $sheet->getStyle('F7:F' . $i)->getNumberFormat()->setFormatCode('#,##');
-        $sheet->getStyle('G7:G' . $i)->getNumberFormat()->setFormatCode('#,##');
-        $sheet->getStyle('E7:E' . $i)->getNumberFormat()->setFormatCode('#,##0.00');
+        $sheet->getStyle('F13:F' . $i)->getNumberFormat()->setFormatCode('#,##');
+        $sheet->getStyle('G13:G' . $i)->getNumberFormat()->setFormatCode('#,##');
+        $sheet->getStyle('E13:E' . $i)->getNumberFormat()->setFormatCode('#,##0.00');
 
 //Set text bold.
         $sheet->getStyle('E' . $i . ':G' . $i)->getFont()->setBold(true);
-        $sheet->getStyle('A7:G' . $i - 1)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
+        $sheet->getStyle('A13:G' . $i - 1)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
         $sheet->getStyle('E' . $i . ':G' . $i)->getBorders()->getAllBorders()->setBorderStyle(\PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN);
 
 
-        $sheet->getStyle('B7:' . 'B' . $i)->getAlignment()->setHorizontal('center');
+        $sheet->getStyle('B13:' . 'B' . $i)->getAlignment()->setHorizontal('center');
         include "convert_number_to_words.php";
 
 
@@ -137,7 +157,7 @@ function phieuxuatkho($listId,$userID)
         $sheet->setCellValue('A' . $i + 3, "Ghi Chú: ");
 
         $u = $i + 5;
-        $sheet->setCellValue('B' . $i + 5, "Người Xuất Phiếu");
+        $sheet->setCellValue('B' . $i + 5, "Người Lập Phiếu");
         $sheet->setCellValue('B' . $i + 6, "(Ký và ghi rõ họ tên)");
         $sheet->getStyle('B' . $u . ':E' . $u)->getFont()->setBold(true);
         $sheet->getStyle('B' . $u . ':E' . $u)->getAlignment()->setHorizontal('center');
@@ -151,8 +171,13 @@ function phieuxuatkho($listId,$userID)
         $sheet->getStyle('B' . $u . ':F' . $u)->getFont()->setBold(true);
         $sheet->getStyle('B' . $u . ':F' . $u)->getAlignment()->setHorizontal('center');
         $sheet->getStyle('B' . $k . ':F' . $k)->getAlignment()->setHorizontal('center');
-        $sheet->setCellValue('A' . $i + 12, "*Kiểm tra hàng kỹ trước khi rời khỏi kho");
-        $sheet->setCellValue('A' . $i + 13, "*Hàng không tên sẽ thanh lý sau 15 ngày kể từ ngày nhận");
+        $sheet->mergeCells('A' . (string)($i + 13) . ':G' . (string)($i + 14));
+        $sheet->setCellValue('A' . $i + 12, "*Kiểm tra hàng kỹ trước khi rời khỏi kho.");
+        $sheet->setCellValue('A' . $i + 13, "*Trung Hoa Logistics chỉ giải quyết khiếu nại trong vòng 02 ngày làm việc từ lúc giao hàng thành công.Nếu quá thời gian trên 
+        Trung Hoa Logistics xin phép không giải quyết những vấn đề liên quan tới việc mất hàng hoặc thiếu hàng.Trân trọng!");
+        $sheet->setCellValue('A' . $i + 15, "*Hàng không tên sẽ thanh lý sau 15 ngày kể từ ngày nhận.");
+        $sheet->getStyle('A' . (string)($i + 13))->getAlignment()->setWrapText(true);
+        $sheet->getStyle('A' . (string)($i + 13) . ':G' . (string)($i + 14))->getAlignment()->setHorizontal('left');
 
         $sheet->setCellValue('B' . $i + 10, mb_strtoupper("Nguyễn Trung Hiếu", "UTF-8"));
         $sheet->setCellValue('F' . $i + 10, mb_strtoupper($kh['fullname'], "UTF-8"));
@@ -162,7 +187,7 @@ function phieuxuatkho($listId,$userID)
         $sheet->mergeCells('F' . $k . ':G' . $k);
         $sheet->mergeCells('F' . (string)($k + 4) . ':G' . (string)($k + 4));
 
-        $sheet->setBreak('A' . $i + 14, \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::BREAK_ROW);
+        $sheet->setBreak('A' . $i + 18, \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::BREAK_ROW);
 //    $sheet->setPrintGridlines(true);
 
 //            echo  print_r($listId,true);
